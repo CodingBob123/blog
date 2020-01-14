81c9bf928ff6cb937ff50296534f25bc145b5c51
qiruihua <qiruihua@live.cn>
2020-01-14 12:12:01 +0800

    用户信息显示

--- blog/users/views.py
+++ blog/users/views.py
@@ -340,5 +340,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class UserCenterView(LoginRequiredMixin,View):
 
     def get(self,request):
-
-        return render(request,'center.html')
+        # 获得登录用户的信息
+        user=request.user
+        #组织获取用户的信息
+        context = {
+            'username':user.username,
+            'mobile':user.mobile,
+            'avatar':user.avatar.url if user.avatar else None,
+            'user_desc':user.user_desc
+        }
+        return render(request,'center.html',context=context)
